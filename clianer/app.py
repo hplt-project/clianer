e5e1fc58cdae3f815149a30ee80093612504b873
Jindra Helcl <helcl@ufal.mff.cuni.cz>
2023-11-13 01:15:46 +0100

    remove old curses components

--- clianer/app.py
+++ clianer/app.py
@@ -8,6 +8,18 @@ from clianer.widgets.main_frame import ClianerFrame, PALETTE
 
 DATA_LOCATION = "/home/helcl/hplt/OpusCleaner/data/train-parts"
 
+def get_datasets(location):
+    datasets = []
+
+    for root, _, paths in os.walk(location):
+        for path in paths:
+            filename = os.path.join(root, path)
+            name = filename[len(location):]
+
+            datasets.append(filename)
+
+    return datasets
+
 
 class App:
     def __init__(self, args):
