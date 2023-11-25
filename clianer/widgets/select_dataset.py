3d31e798c8af6a031f6f794f466caa23ea92553f
Jindra Helcl <helcl@ufal.mff.cuni.cz>
2023-11-25 18:51:19 +0100

    show star with datasets that already belong to some categories

--- clianer/widgets/select_dataset.py
+++ clianer/widgets/select_dataset.py
@@ -6,13 +6,21 @@ from clianer.widgets.button import CustomButton
 from opuscleaner.config import DATA_PATH
 from opuscleaner.datasets import list_datasets
 from opuscleaner.server import api_get_dataset_filters
-
+from opuscleaner.categories import get_mapping
 
 class SelectDatasetDialog(Dialog):
 
     def __init__(self):
         # datasets: Dict[str, List[Tuple[str, Path]]]
         self.datasets = list_datasets(DATA_PATH)
+        category_mapping = get_mapping()
+
+        have_mapping = []
+        for cat in category_mapping.categories:
+            if cat.name in category_mapping.mapping:
+                have_mapping.extend(category_mapping.mapping[cat.name])
+
+        have_mapping = set(have_mapping)
 
         self.dataset_widgets = []
         for dataset_name in self.datasets:
@@ -24,6 +32,9 @@ class SelectDatasetDialog(Dialog):
             pipeline = api_get_dataset_filters(dataset_name)
             if pipeline.filters:
                 label += f" [{len(pipeline.filters)}]"
+
+            if dataset_name in have_mapping:
+                label = f"* {label}"
 
             button = CustomButton(
                 label, on_press=self.select_dataset, user_data=dataset_name)
